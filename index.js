0c9373e9ebbffb472df00a67c58730b973840365
Nabil-Shartaj-Khan <shartajnabil@gmail.com>
2023-10-11 13:07:28 +0600

    Circle

--- index.js
+++ index.js
@@ -7,11 +7,22 @@
 
 // document.getElementById("test").innerHTML="Test is overrode by "+ my_name;
 
-//
-let username;
-document.getElementById("newButton").onclick=function(){
-    username=document.getElementById("newText").value;
-    console.log(username);
-    document.getElementById("newlabel").innerHTML="Hello " + username
+
+// let username;
+// document.getElementById("newButton").onclick=function(){
+//     username=document.getElementById("newText").value;
+//     console.log(username);
+//     document.getElementById("newlabel").innerHTML="Hello " + username
     
-}
+// }
+
+//calculating circle hypotenuse
+// document.getElementById("cibutton").onclick = function () {
+//     value_a = Number(document.getElementById("avalue").value);
+//     value_b = Number(document.getElementById("bvalue").value);
+
+//     value_c = Math.sqrt(Math.pow(value_a, 2) + Math.pow(value_b, 2));
+
+//     document.getElementById("result").innerHTML = "Side C is" + value_c;
+// }
+
