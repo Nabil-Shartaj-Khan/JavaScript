e60539c450d7a745f4e6b338aa4ed39b73cb259d
Nabil-Shartaj-Khan <shartajnabil@gmail.com>
2024-02-10 23:22:49 +0600

    getter setter

--- OOP.js
+++ OOP.js
@@ -3,14 +3,13 @@ let Person = function (name, gender, birthYear) {
   this.name = name;
   this.gender = gender;
   this.birthYear = birthYear;
-
 };
 
 //prototyping as calcage is needed by every instance
-Person.prototype.calcAge={
-    return new Date().getFullYear() - this.birthYear;
-  };
-
+// Prototype method to calculate age
+Person.prototype.calcAge = function () {
+  return new Date().getFullYear() - this.birthYear;
+};
 //first object
 let nabil = new Person("Nabil", "Male", 1999);
 console.log(nabil.calcAge());
@@ -21,3 +20,59 @@ console.log(shartaz.calcAge());
 
 console.log(nabil);
 console.log(shartaz);
+
+//javascript class
+
+class Man {
+  constructor(name, birthYear, gender) {
+    this.name = name;
+    this.birthYear = birthYear;
+    this.gender = gender;
+  }
+
+  calcAge() {
+    console.log(new Date().getFullYear() - this.birthYear);
+  }
+}
+
+let john = new Man("John", 1995, "Male");
+console.log(john);
+
+//getter setter for objects
+
+let Khan = {
+  name: "Nabil",
+  age: 24,
+  salary: 12000,
+
+  get getSalary() {
+    return "Tk: " + this.salary;
+  },
+
+  set setName(val) {
+    this.name = val;
+  },
+};
+
+console.log(Khan.getSalary);
+
+//getter setter for class
+
+let User = class {
+  constructor(name, pass, email) {
+    this.name = name;
+    this.pass = pass;
+    this.email = email;
+  }
+  set setPass(val) {
+    if (val.length < 4) {
+      console.log("Password should be more than 4 characters!");
+    }
+
+    this.pass = val;
+  }
+};
+
+let mark = new User("Mark", "123456", "mark@mark.com");
+mark.setPass = "123456Mark";
+console.log(mark);
